a91ecf695d69af03114e46c866438c2f3309157f
sportanova <sportano@gmail.com>
2014-11-26 12:55:02 -0800

    laser direction - WIP

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -1,14 +1,6 @@
-/**
- * Player Entity
- */
+var playerData = {};
+
 game.PlayerEntity = me.Entity.extend({
- 
-    /* -----
- 
-    constructor
- 
-    ------ */
- 
     init: function(x, y, settings) {
         settings['height'] = 75;
         // call the constructor
@@ -37,21 +29,25 @@ game.PlayerEntity = me.Entity.extend({
             this.renderable.angle = Math.PI * 1.75;
             this.body.vel.y -= this.body.accel.y * me.timer.tick;
             this.body.vel.x -= this.body.accel.x * me.timer.tick;
+            playerData.orientation = 'northWest';
         }
         else if (me.input.isKeyPressed('left') && me.input.isKeyPressed('down')) {
             this.renderable.angle = Math.PI * -0.75;
             this.body.vel.y += this.body.accel.y * me.timer.tick;
             this.body.vel.x -= this.body.accel.x * me.timer.tick;
+            playerData.orientation = 'southWest';
         }
         else if (me.input.isKeyPressed('right') && me.input.isKeyPressed('up')) {
             this.renderable.angle = Math.PI * 0.25;
             this.body.vel.y -= this.body.accel.y * me.timer.tick;
             this.body.vel.x += this.body.accel.x * me.timer.tick;
+            playerData.orientation = 'northEast';
         }
         else if (me.input.isKeyPressed('right') && me.input.isKeyPressed('down')) {
             this.renderable.angle = Math.PI * -1.25;
             this.body.vel.y += this.body.accel.y * me.timer.tick;
             this.body.vel.x += this.body.accel.x * me.timer.tick;
+            playerData.orientation = 'southEast';
         }
         else if (me.input.isKeyPressed('left')) {
             // flip the sprite on horizontal axis
@@ -60,6 +56,7 @@ game.PlayerEntity = me.Entity.extend({
             this.renderable.angle = Math.PI * -0.5;
             this.body.vel.x -= this.body.accel.x * me.timer.tick;
             this.body.vel.y = 0;
+            playerData.orientation = 'west';
             // change to the walking animation
             // if (!this.renderable.isCurrentAnimation("walk")) {
             //     this.renderable.setCurrentAnimation("walk");
@@ -73,16 +70,19 @@ game.PlayerEntity = me.Entity.extend({
             // this.renderable.flipY(true);
             // update the entity velocity
             this.body.vel.x += this.body.accel.x * me.timer.tick;
+            playerData.orientation = 'east';
         }
         else if (me.input.isKeyPressed('up')) {
             this.body.vel.y -= this.body.accel.y * me.timer.tick;
             this.body.vel.x = 0;
             this.renderable.angle = 0;
+            playerData.orientation = 'north';
         }
         else if (me.input.isKeyPressed('down')) {
             this.body.vel.y += this.body.accel.y * me.timer.tick;
             this.body.vel.x = 0;
             this.renderable.angle = Math.PI;
+            playerData.orientation = 'south';
         }
         else {
             // this.body.vel.x = 0;
@@ -129,18 +129,71 @@ game.PlayerEntity = me.Entity.extend({
 
 game.Laser = me.Renderable.extend({
     init: function (x, y) {
-        this._super(me.Renderable, "init", [x, y, game.Laser.width, game.Laser.height]);
+        var lazerCords = this.calcLaserOrientation(x, y, playerData.orientation);
+        this._super(me.Renderable, "init", [lazerCords.x, lazerCords.y, game.Laser.width, game.Laser.height]);
         this.z = 5;
-        this.body = new me.Body(this, ([
-            new me.Polygon(0, 0, [
-                new me.Vector2d(0, 0),
-                new me.Vector2d(this.width, 0),
-                new me.Vector2d(this.width, this.height),
-                new me.Vector2d(0, this.height)
-            ])
-        ]));
+        var body = this.calcLaserShape();
+        this.body = body;
+
+        // this.body = new me.Body(this, ([
+        //     new me.Polygon(0, 0, [
+        //         new me.Vector2d(0, 0),
+        //         new me.Vector2d(this.width, 0),
+        //         new me.Vector2d(this.width, this.height),
+        //         new me.Vector2d(0, this.height)
+        //     ])
+        // ]));
+        console.log('this.body', this.body.entity)
+        console.log('this.body', this.body.entity.body.shapes[0].points)
         this.body.updateBounds();
         this.body.setVelocity(0, 20);
+    },
+
+    calcLaserOrientation: function(x, y, playerOrientation) {
+      if(playerOrientation === 'south') {
+        return {x: x, y: y + 110};
+      }
+      if(playerOrientation === 'north') {
+        return {x: x, y: y - 10};
+      }
+      if(playerOrientation === 'west') {
+        console.log('getting here')
+        return {x: x - 60, y: y + 40};
+      }
+      else {
+        return {x: x, y: y}
+      }
+    },
+    calcLaserShape: function() {
+      return new me.Body(this, ([
+            new me.Polygon(0, 0, [
+                new me.Vector2d(-this.height, 0),
+                new me.Vector2d(this.height, 0),
+                new me.Vector2d(-this.height, this.width),
+                new me.Vector2d(this.height, this.width)
+            ])
+        ]));
+    },
+    calcLaserDirectionDelta: function(velx, vely, accelX, accelY, playerOrientation, tick) {
+      if(playerOrientation === 'south') {
+        this.body.setVelocity(0, 20);
+        var updatedVelX = 0;
+        var updatedVelY = vely + (accelY * tick);
+        return {velx: updatedVelX, vely: updatedVelY};
+      }
+      if(playerOrientation === 'north') {
+        this.body.setVelocity(0, 20);
+        var updatedVelX = 0;
+        var updatedVelY = vely - (accelY * tick);
+        return {velx: updatedVelX, vely: updatedVelY};
+      }
+      if(playerOrientation === 'west') {
+        this.body.setVelocity(20, 0);
+        var updatedVelX = velx - (accelX * tick);
+        var updatedVelY = 0;
+        console.log('updatedVelY', updatedVelY)
+        return {velx: updatedVelX, vely: updatedVelY};
+      }
     },
 
     draw: function (renderer) {
@@ -152,7 +205,13 @@ game.Laser = me.Renderable.extend({
 
     update: function (time) {
         this._super(me.Renderable, "update", [time]);
-        this.body.vel.y -= this.body.accel.y * me.timer.tick;
+        var deltas = this.calcLaserDirectionDelta(this.body.vel.x, this.body.vel.y, this.body.accel.x, this.body.accel.y, playerData.orientation, me.timer.tick);
+        // this.body.vel.y += this.body.accel.y * me.timer.tick;
+        this.body.vel.y = deltas.vely;
+        this.body.vel.x = deltas.velx;
+        // console.log('this.body.vel.y', this.body.vel.y);
+        // this.body.vel.x -= this.body.accel.y * me.timer.tick;
+
         if (this.pos.y + this.height <= 0) {
             me.game.world.removeChild(this);
         }
@@ -163,8 +222,8 @@ game.Laser = me.Renderable.extend({
     },
     onCollision: function(response, other) {
         if(response.a.name === 'mainplayer') {
-            console.log('response', response)
-            console.log('other', other)
+            // console.log('response', response)
+            // console.log('other', other)
             return false;
         }
         return true;
